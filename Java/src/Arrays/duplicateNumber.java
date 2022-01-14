2762291bdce974ae8f58c4d984ea32237180b675
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2022-01-14 22:27:10 +0530

    Day 5

---
+++ Java/src/Arrays/duplicateNumber.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class duplicateNumber {
+    public static void main(String[] args) {
+        int[] arr= {3,1,3,4,2};
+        System.out.println(findDuplicateOpt(arr));
+    }
+
+    //nlogn
+    static int findDuplicate(int[] nums){
+        int n= nums.length-1;
+        Arrays.sort(nums);
+        for (int i = 0; i <= n; i++) {
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    //optimised solution- O(n)
+    static int findDuplicateOpt(int[] nums){
+        Set<Integer> s= new HashSet<>();
+        for (int i:nums){
+            if(s.contains(i)){
+                return i;
+            }
+            s.add(i);
+        }
+        return -1;
+    }
+}
